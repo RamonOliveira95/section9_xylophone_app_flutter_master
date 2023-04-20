68e5d73eb7311d56876d03e43d8b2e24b71ad44c
RamonOliveira95 <ramonsilvadeoliveira95@hotmail.com>
2023-04-20 16:45:38 -0300

    1.0.1

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 # AULA 1 - 1.0.0
 - Foi adicionado o package audioplayers 3.0.0;
 - Criado um TextButton que receba um toque e no setState chamad a função do package que emita o som.
+
+# AULA 2 - 1.0.1
+- Adicionado alguns Container, cada um com seu respectivo som e cor;
+- Criado uma Function para chamar e informar para ela apenas o número do áudio que tem que tocar.
--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,80 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    playSound(int soundNumber) {
+      final player = AudioCache();
+      player.load('note$soundNumber.wav');
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.load('note1.wav');
-              },
-              child: Text("Clique aqui"),
-            ),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.red,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Text(""),
+                ),
+              ),
+              Container(
+                color: Colors.orange,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Text(""),
+                ),
+              ),
+              Container(
+                color: Colors.yellow,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Text(""),
+                ),
+              ),
+              Container(
+                color: Colors.green,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Text(""),
+                ),
+              ),
+              Container(
+                color: Colors.teal,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Text(""),
+                ),
+              ),
+              Container(
+                color: Colors.blue,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Text(""),
+                ),
+              ),
+              Container(
+                color: Colors.purple,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Text(""),
+                ),
+              ),
+            ],
           ),
         ),
       ),
